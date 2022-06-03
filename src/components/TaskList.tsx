192f68141ad05d5519b409b2da55d24f3a9a52df
santirequelme <65666330+santirequelme@users.noreply.github.com>
2022-06-03 11:27:46 -0300

    🚀 Desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,6 +30,12 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+          ...task,
+          isComplete: !task.isComplete
+          }: task);
+    setTasks(newTasks);
+    
   }
 
   function handleRemoveTask(id: number) {
